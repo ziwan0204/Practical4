7bbacf9c17c14df9fd947548b4df461f0ec79956
Zi Wan <yipzw-wm17@student.tarc.edu.my>
2019-06-23 23:17:13 +0800

    Practical 4

---
+++ app/src/main/java/com/example/p4/ChildActivity.java
@@ -0,0 +1,62 @@
+package com.example.p4;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_child);
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_child_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        // ...
+
+        // Define the listener
+        OnActionExpandListener expandListener = new OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Do something when action item collapses
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Do something when expanded
+                return true;  // Return true to expand action view
+            }
+        };
+
+        // Get the MenuItem for the action item
+        MenuItem actionMenuItem = menu.findItem(R.id.myActionItem);
+
+        // Assign the listener to that action item
+        MenuItemCompat.setOnActionExpandListener(actionMenuItem, expandListener);
+
+        // Any other things you have to do when creating the options menu...
+
+        return true;
+    }
+    private void setSupportActionBar(Toolbar myChildToolbar) {
+    }
+}
---
+++ app/src/main/java/com/example/p4/MenuItemCompat.java
@@ -0,0 +1,8 @@
+package com.example.p4;
+
+import android.view.MenuItem;
+
+class MenuItemCompat {
+    public static void setOnActionExpandListener(MenuItem actionMenuItem, MenuItem.OnActionExpandListener expandListener) {
+    }
+}
